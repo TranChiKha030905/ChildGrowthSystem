5eff8491555dbbeeb13e859c265f68b7f1c61151
huyvu0607 <vudh1092@ut.edu.vn>
2025-03-18 01:47:18 +0700

    add login.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,26 +9,6 @@
 
 (function() {
   "use strict";
-  const wrapper = document.querySelector('.wrapper');
-  const loginLink = document.querySelector('.login-link');
-  const registerLink = document.querySelector('.register-link');
-  const btnPopup = document.querySelector('.btnLogin-popup');
-  const iconClose = document.querySelector('.icon-close');
-  const overLay = document.querySelector('.overlay');
-  
-  
-  registerLink.addEventListener('click', ()=>{
-      wrapper.classList.add('active');
-  })
-  
-  loginLink.addEventListener('click', ()=>{
-      wrapper.classList.remove('active');
-  })
-  
-  iconClose.addEventListener('click',()=>{
-      wrapper.classList.remove('active-popup');
-      overLay.classList.remove('active');
-  } )
   /**
    * Apply .scrolled class to the body as the page is scrolled down
    */
