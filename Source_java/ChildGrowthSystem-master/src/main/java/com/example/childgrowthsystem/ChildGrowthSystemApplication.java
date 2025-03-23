d44f5819763da46f95bec76ddfa978b84f4e02ee
huyvu0607 <vudh1092@ut.edu.vn>
2025-03-23 19:35:20 +0700

    add template index to javaspring

---
+++ Source_java/ChildGrowthSystem-master/src/main/java/com/example/childgrowthsystem/ChildGrowthSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.childgrowthsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildGrowthSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChildGrowthSystemApplication.class, args);
+    }
+
+}
