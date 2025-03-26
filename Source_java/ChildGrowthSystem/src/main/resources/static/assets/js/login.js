f565ab231308c3524ce6002e10af633cd655e519
huyvu0607 <vudh1092@ut.edu.vn>
2025-03-27 00:36:50 +0700

    add page admin and page feedback

---
+++ Source_java/ChildGrowthSystem/src/main/resources/static/assets/js/login.js
@@ -0,0 +1,20 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+const overLay = document.querySelector('.overlay');
+
+
+registerLink.addEventListener('click', ()=>{
+    wrapper.classList.add('active');
+})
+
+loginLink.addEventListener('click', ()=>{
+    wrapper.classList.remove('active');
+})
+
+iconClose.addEventListener('click',()=>{
+    wrapper.classList.remove('active-popup');
+    overLay.classList.remove('active');
+} )
