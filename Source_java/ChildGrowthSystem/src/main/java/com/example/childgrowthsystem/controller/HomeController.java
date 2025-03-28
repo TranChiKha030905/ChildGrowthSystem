e84ec02ecfd6b029ba8539f389afcae1b2cb14d7
huyvu0607 <vudh1092@ut.edu.vn>
2025-03-28 19:32:55 +0700

    add page service and pricing

--- Source_java/ChildGrowthSystem/src/main/java/com/example/childgrowthsystem/controller/HomeController.java
+++ Source_java/ChildGrowthSystem/src/main/java/com/example/childgrowthsystem/controller/HomeController.java
@@ -1,20 +1,30 @@
 package com.example.childgrowthsystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/index")
+    @GetMapping("/index")
     public String index() {
         return "index";
     }
 
 
-    @RequestMapping("/feedback")
+    @GetMapping("/feedback")
     public String feedback() {
         return "feedback";
     }
 
+    @GetMapping("/pricing")
+    public String pricing() {
+        return "pricing";
+    }
+
+    @GetMapping("/service")
+    public String service() {
+        return "service";
+    }
 }
