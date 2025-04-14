ed0aea2dbec42b722414fbcfb3efe30615e11182
TuanNguyenHungCNTTUTH <hungnt1692@ut.edu.vn>
2025-04-14 23:00:07 +0700

    cap nhat file reponsitory

---
+++ ChildGrowthSystem/src/main/java/com/childgrowth/tracking/repository/AdviceRequestRepository.java
@@ -0,0 +1,9 @@
+package com.childgrowth.tracking.repository;
+
+import com.childgrowth.tracking.model.AdviceRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface AdviceRequestRepository extends JpaRepository<AdviceRequest, Long> {
+    List<AdviceRequest> findByResolved(boolean resolved);
+}
---
+++ ChildGrowthSystem/src/main/java/com/childgrowth/tracking/repository/ChildProfileRepository.java
@@ -0,0 +1,10 @@
+// ChildProfileRepository.java
+package com.childgrowth.tracking.repository;
+
+import com.childgrowth.tracking.model.ChildProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ChildProfileRepository extends JpaRepository<ChildProfile, Long> {
+    List<ChildProfile> findByUserId(Long userId);
+}
