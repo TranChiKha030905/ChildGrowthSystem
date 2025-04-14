fe43e5be2be9082420fd8feccde416c2f18cc522
TuanNguyenHungCNTTUTH <hungnt1692@ut.edu.vn>
2025-04-14 22:58:22 +0700

    thay doi

--- ChildGrowthSystem/src/main/java/com/example/childgrowthsystem/repository/AdviceRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.childgrowthsystem.repository;
-
-import com.childgrowth.tracking.model.AdviceRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface AdviceRequestRepository extends JpaRepository<AdviceRequest, Long> {
-    List<AdviceRequest> findByResolved(boolean resolved);
-}
--- ChildGrowthSystem/src/main/java/com/example/childgrowthsystem/repository/MembershipPlanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.childgrowthsystem.repository;
-
-import com.childgrowth.tracking.model.MembershipPlan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MembershipPlanRepository extends JpaRepository<MembershipPlan, Long> {
-}
